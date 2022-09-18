4adb6dac0cfd679b8dfbee825280c217f0e2be65
gimli <bwoods51501@gmail.com>
2022-09-17 20:39:11 -0400

    Most of TestQueue except front

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -5,5 +5,84 @@
 // Assignment: 5
 // IDE Name: IntelliJ
 
-public class Queue {
+public class Queue<E>
+{
+    public Node<E> head, tail;
+
+    //constructor method to create a list of object with head, tail, and size.
+    public Queue()
+    {
+        head = null;
+        tail = null;
+
+    }
+
+    //method add node to end of list
+    public void enqueue(E data)
+    {
+        if (tail == null)
+            head = tail = new Node<E>(data); //empty list
+        else
+        {
+            tail.next = new Node<E>(data); //link new node as last node
+            tail = tail.next; //make tail pointer points to last node
+        }
+    }
+
+    //method #3: remove first node
+    public void dequeue()
+    {
+        if (head == null)
+            return;
+        else {
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                head = head.next;
+            }
+        }
+    }
+
+    //method Size
+    public int size() {
+        int listSize= 0;
+
+        Node<E> temp = head;
+
+        while (temp != null)
+        {
+            listSize++;
+            temp = temp.next;
+        }
+
+        return listSize;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    //method to print out the list
+    public void printQueue()
+    {
+        Node<E> temp;
+        temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data + "   ");
+            temp = temp.next;
+        }
+    }
+
+    //class to create nodes as objects
+    private static class Node<E> {
+        private E data;  //data field
+        private Node<E> next; //link field
+        public Node(E item) //constructor method
+        {
+            data = item;
+            next = null;
+        }
+    }
 }
--- src/main/java/TestQueue.java
+++ src/main/java/TestQueue.java
@@ -5,5 +5,91 @@
 // Assignment: 5
 // IDE Name: IntelliJ
 
+/**
+ * Need to add the front method
+ * */
+
+import java.util.Scanner;
+
 public class TestQueue {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        Queue<Integer> queue = new Queue();
+
+        int option, data;
+
+        while(true) {
+
+            printMenu();
+            option = input.nextInt();
+
+            switch (option) {
+                case 1: // Enqueue element
+                    System.out.print("\nEnter the number to enqueue: ");
+                    data = input.nextInt();
+
+                    System.out.println("\nEnqueueing " + data);
+                    System.out.println("Queue content before enqueueing " + data + " is: ");
+                    queue.printQueue();
+                    queue.enqueue(data);
+
+                    System.out.print("\nQueue content after enqueueing " + data + " is: ");
+                    queue.printQueue();
+                    System.out.println();
+                    break;
+                case 2: // Deque element
+                    System.out.print("Queue content before dequeueing is:");
+                    queue.printQueue();
+
+                    queue.dequeue();
+
+                    System.out.print("\nQueue content before dequeueing is:");
+                    queue.printQueue();
+                    System.out.println();
+                    break;
+                case 3: // Front Element
+                    break;
+                case 4: // Size of Queue
+                    System.out.println("The Size of the Queue with contents:");
+                    queue.printQueue();
+                    System.out.print(" is: " + queue.size());
+                    break;
+                case 5: // is Empty Queue
+                    if (queue.isEmpty())
+                        System.out.println("\nThe Queue is empty.");
+                    else {
+                        System.out.println("\nThe Queue is Not empty.");
+
+                        System.out.println("\nThe content of the queue is: ");
+                        queue.printQueue();
+                        System.out.println();
+                    }
+                    break;
+                case 6: // Print queue content
+                    System.out.print("\nThe content of the queue is:");
+                    queue.printQueue();
+                    System.out.println();
+                    break;
+                case 7: // Exit
+                    System.exit(-1);
+                    break;
+            }
+        }
+    }
+
+    public static void printMenu() {
+
+        System.out.println("\n--------MAIN MENU--------");
+        System.out.println("1 - Enqueue element");
+        System.out.println("2 - Dequeue element");
+        System.out.println("3 – Front element");
+        System.out.println("4 – Size of queue");
+        System.out.println("5 – Is Empty queue?");
+        System.out.println("6 - Print queue content");
+        System.out.println("7 - Exit program");
+        System.out.print("\nEnter option number: ");
+    }
 }
