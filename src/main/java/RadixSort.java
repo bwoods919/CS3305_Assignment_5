a118c830f48e02068ca6db83c3359ff018164de6
gimli <bwoods51501@gmail.com>
2022-09-24 15:04:34 -0400

    Finally Radixsort is done

--- src/main/java/RadixSort.java
+++ src/main/java/RadixSort.java
@@ -5,126 +5,98 @@
 // Assignment: 5
 // IDE Name: IntelliJ
 
-import java.lang.runtime.SwitchBootstraps;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.*;
 
-public class RadixSort {
+class Radixsort {
+    static int getMax(int arr[], int n)
+    {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
 
-    public static void main(String[] args) {
+    // A function to do counting sort of arr[] according to the digit represented by exp.
+    static void countSort(int arr[], int n, int exp)
+    {
+        int output[] = new int[n]; // output array
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count, 0);
 
-        Scanner input = new Scanner(System.in);
+        // Store count of occurrences in count[]
+        for (i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
 
-        String exit;
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
 
-        int size;
-        int array[] = new int[100];
+        // Build the output array
+        for (i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
 
-        // Sentient Loop
-        while (true) {
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
 
-            // Read in user input
-            System.out.print("Enter the size of the array: ");
-            size = input.nextInt();
+    // The main function to that sorts arr[] of size n using Radix Sort
+    static void radixsort(int arr[], int n) {
 
-            System.out.print("Enter the numbers to sort: ");
+        // Find the maximum number to know number of digits
+        int m = getMax(arr, n);
 
-            for (int i = 0; i < size; i++) {
-                array[i] = input.nextInt();
+        for (int exp = 1; m / exp > 0; exp *= 10)
+            countSort(arr, n, exp);
+    }
+
+    // A utility function to print an array
+    static void print(int arr[], int n)
+    {
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
+
+// Main class
+class Main {
+    public static void main(String args[]) {
+
+        while(true){
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("How many integers in the array: ");
+            int a= input.nextInt();
+            int[] data = new int[a];
+
+            System.out.println("Enter the elements of the array: ");
+
+            for(int i=0; i<a; i++)
+            {
+                //reading array elements from the user
+                data[i]=input .nextInt();
             }
+            System.out.println("Array elements are: ");
 
-            // Output the array before sorting
-            System.out.print("The array before sorting: ");
-            for (int i = 0; i < size; i++) {
-                System.out.print( array[i] + " ");
-            }
-            System.out.println();
+            System.out.println("Inputs array before sorting: ");
+            System.out.println(Arrays.toString(data));
 
-            // Sort the array
-            array = radixSort(array, size);
+            int size = data.length;
 
-            // Output the array after sorting
-            System.out.print("The array after sorting: ");
-            for (int i = 0; i < size; i++) {
-                System.out.print(array[i] + " ");
-            }
+            Radixsort.radixsort(data,size);
 
-            // Code to handle the exit logic
-            System.out.print("\nDo you want to re-run code with different input string (Y/N)? ");
-            exit = input.next();
+            System.out.println("Inputs array after sorting: ");
+            System.out.println(Arrays.toString(data));
+
+            System.out.println("Do you want to run again? (Y/N): ");
+            String exit = input.next();
+            exit = exit.toLowerCase();
 
             if (exit.equals("N") || exit.equals("n") || exit.equals("No") || exit.equals("no") || exit.equals("NO"))
                 System.exit(255);
         }
-    }
-
-    // Method to figure out the digit based on the position given as digit on input
-    public static int ExtractDigit(int num, int digit) {
-
-        return 0;
-    }
-
-    /*
-    * 1 - Extract digit
-    * 2 - Sort into the corresponding Queue
-    * 3 - Go to next digit
-    * 4 - repeat 1-3 till no more digits
-    * 5 - return array
-    */
-
-    // Method to do the actual sort
-    public static int[] radixSort(int[] array, int size) {
-
-        // Declare the Queues java was not liking trying to make an array of Queues
-        Queue<Integer> Q0 = new Queue<>();
-        Queue<Integer> Q1 = new Queue<>();
-        Queue<Integer> Q2 = new Queue<>();
-        Queue<Integer> Q3 = new Queue<>();
-        Queue<Integer> Q4 = new Queue<>();
-        Queue<Integer> Q5 = new Queue<>();
-        Queue<Integer> Q6 = new Queue<>();
-        Queue<Integer> Q7 = new Queue<>();
-        Queue<Integer> Q8 = new Queue<>();
-        Queue<Integer> Q9 = new Queue<>();
-
-        for (int i = 0; i < 10; i++) { // Digits
-            for (int j = 0; j < size; j++) { // Size
-                switch (ExtractDigit(array[j], i)) { // add to the correct Queue
-                    case 0: // the digit returns as 0
-                        Q0.enqueue(array[j]);
-                        break;
-                    case 1: // the digit returns as 1
-                        Q1.enqueue(array[j]);
-                        break;
-                    case 2: // the digit returns as 2
-                        Q2.enqueue(array[j]);
-                        break;
-                    case 3: // the digit returns as 3
-                        Q3.enqueue(array[j]);
-                        break;
-                    case 4: // the digit returns as 4
-                        Q4.enqueue(array[j]);
-                        break;
-                    case 5: // the digit returns as 5
-                        Q5.enqueue(array[j]);
-                        break;
-                    case 6: // the digit returns as 6
-                        Q6.enqueue(array[j]);
-                        break;
-                    case 7: // the digit returns as 7
-                        Q7.enqueue(array[j]);
-                        break;
-                    case 8: // the digit returns as 8
-                        Q8.enqueue(array[j]);
-                        break;
-                    case 9: // the digit returns as 9
-                        Q9.enqueue(array[j]);
-                        break;
-                }
-
-                // Logic to empty the queues from 0 to 9
-            }
-        }
-
-        return array;
     }
 }
